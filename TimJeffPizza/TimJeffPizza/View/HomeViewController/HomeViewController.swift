417946826b70d1e26b9f4a520f5441a609bc412c
thommzy <tomiwaobeisun@yahoo.com>
2021-10-29 09:08:19 +0100

    injected the method from the viemodel into my viewcontroller to get the data from the endpoint.

--- TimJeffPizza/TimJeffPizza/View/HomeViewController/HomeViewController.swift
+++ TimJeffPizza/TimJeffPizza/View/HomeViewController/HomeViewController.swift
@@ -8,8 +8,26 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    let appViewModel = AppViewModel(dataService: NetworkService())
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        appViewModel.delegate = self
+        appViewModel.getPizzaList()
+    }
+}
+
+extension HomeViewController: GetListOfPizzaProtocol {
+    func listPizza(pizza: [JeffPizzaListResponseModel]?) {
+        if let pizza = pizza {
+            print(pizza.count,"--->>>pizzaCount")
+            print(pizza,"--->>>pizzaList")
+        }
+    }
+    
+    func errorMessage(error: Error?) {
+        if let error = error {
+            print("error--->>> \(error)")
+        }
     }
 }
