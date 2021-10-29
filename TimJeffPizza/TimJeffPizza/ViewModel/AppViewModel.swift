77122225c317cf14bf12afb1ad5d2f20756728fd
thommzy <tomiwaobeisun@yahoo.com>
2021-10-29 09:07:44 +0100

    implemented the app viemodel

--- TimJeffPizza/TimJeffPizza/ViewModel/AppViewModel.swift
+++ TimJeffPizza/TimJeffPizza/ViewModel/AppViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class AppViewModel {
+
+    private var dataService: NetworkServiceProtocol?
+    var delegate: GetListOfPizzaProtocol?
+    
+    // MARK: - Constructor
+    init(dataService: NetworkServiceProtocol) {
+        self.dataService = dataService
+    }
+    
+    func getPizzaList() {
+        self.dataService?.parse(completion: { result, error in
+            if let error = error {
+                self.delegate?.errorMessage(error: error)
+            }
+            self.delegate?.listPizza(pizza: result ?? [])
+        })
+    }
+}
