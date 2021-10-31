e6a3a8763ecb3df29a00c83e6c0905e5471564ea
thommzy <tomiwaobeisun@yahoo.com>
2021-10-31 19:20:41 +0100

    created Empty PizzaTableViewCell

---
+++ TimJeffPizza/TimJeffPizza/Cells/PizzaTableViewCell/PizzaTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PizzaTableViewCell.swift
+//  TimJeffPizza
+//
+//  Created by Obeisun Timothy on 31/10/2021.
+//
+
+import UIKit
+
+class PizzaTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
