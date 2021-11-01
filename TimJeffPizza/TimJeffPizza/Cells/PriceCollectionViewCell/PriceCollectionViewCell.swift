0fa67b97b1b0ebf9743b980d540ca36ea6247d3e
thommzy <tomiwaobeisun@yahoo.com>
2021-11-01 11:41:36 +0100

    collectioncell for price added

--- TimJeffPizza/TimJeffPizza/Cells/PriceCollectionViewCell/PriceCollectionViewCell.swift
+++ TimJeffPizza/TimJeffPizza/Cells/PriceCollectionViewCell/PriceCollectionViewCell.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class PriceCollectionViewCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var sizeLbl: UILabel!
+    @IBOutlet weak var priceBtn: UIButton!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var parentView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        setupParentView()
     }
-
+    
+    func setupParentView() {
+        parentView.layer.cornerRadius = 10
+        parentView.layer.masksToBounds = true
+    }
+    
+    var priceDetails: Price? {
+        didSet {
+            sizeLbl.text = priceDetails?.size
+            priceLabel.text = "$\(priceDetails?.price ?? Double())"
+        }
+    }
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
 }
