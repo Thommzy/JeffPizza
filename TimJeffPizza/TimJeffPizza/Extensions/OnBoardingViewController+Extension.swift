7a4cc265fce252460545cc7776c01bdb04b3c27d
thommzy <tomiwaobeisun@yahoo.com>
2021-11-03 10:39:36 +0100

    refactored onboarding view controller file

--- TimJeffPizza/TimJeffPizza/Extensions/OnBoardingViewController+Extension.swift
+++ TimJeffPizza/TimJeffPizza/Extensions/OnBoardingViewController+Extension.swift
@@ -10,10 +10,10 @@ import UIKit
 
 extension OnBoardingViewController {
     func setupPresentMainTabBarController(userStatus: Bool, logInStatus: Bool) {
-        UserDefaults.standard.set(userStatus, forKey: "isMarried")
-        UserDefaults.standard.set(logInStatus, forKey: "isLoggedIn")
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
+        UserDefaults.standard.set(userStatus, forKey: Constants.isMarried)
+        UserDefaults.standard.set(logInStatus, forKey: Constants.isLoggedIn)
+        let storyboard = UIStoryboard(name: Constants.main, bundle: nil)
+        let mainTabBarController = storyboard.instantiateViewController(identifier: Constants.mainTabBarController)
         mainTabBarController.modalPresentationStyle = .overFullScreen
         self.present(mainTabBarController, animated: true, completion: nil)
     }
