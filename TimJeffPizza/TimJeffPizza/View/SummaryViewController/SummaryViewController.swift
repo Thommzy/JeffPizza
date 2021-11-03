2440cec435f56ff8e972c87b43c34e6b094f6a60
thommzy <tomiwaobeisun@yahoo.com>
2021-11-03 10:34:18 +0100

    summaryVc swift file

--- TimJeffPizza/TimJeffPizza/View/SummaryViewController/SummaryViewController.swift
+++ TimJeffPizza/TimJeffPizza/View/SummaryViewController/SummaryViewController.swift
@@ -9,21 +9,58 @@ import UIKit
 
 class SummaryViewController: UIViewController {
 
+    @IBOutlet weak var totalaAmountLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var pizzaNameLabel: UILabel!
+    @IBOutlet weak var continueBtn: UIButton!
+    
+    var totalAmount: String?
+    var pizzaName: String?
+    var totalPrice: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = Constants.summary
+        setupDatas()
+        setupContinueBtn()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    func setupDatas() {
+        pizzaNameLabel.text = pizzaName
+        let userType = UserDefaults.standard.bool(forKey: Constants.isMarried)
+        switch userType {
+        case true:
+            statusLabel.text = Constants.married
+        case false:
+            statusLabel.text = Constants.single
+        }
+        totalaAmountLabel.text = totalPrice
     }
-    */
-
+    
+    func setupContinueBtn() {
+        continueBtn.setupViewBorder(cornerRadius: 15, borderWidth: nil, masksToBound: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    @IBAction func continueBtnAction(_ sender: UIButton) {
+        let alert = UIAlertController(title: Constants.appContinue, message: Constants.continueMessage, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: Constants.appContinue, style: UIAlertAction.Style.destructive, handler: { [weak self] action in
+            self?.setupMoveToMainScreen()
+        }))
+        alert.addAction(UIAlertAction(title: Constants.appCancel, style: UIAlertAction.Style.cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func setupMoveToMainScreen() {
+        let storyboard = UIStoryboard(name: Constants.main, bundle: nil)
+        let mainTabBarController = storyboard.instantiateViewController(identifier: Constants.mainTabBarController)
+        mainTabBarController.modalPresentationStyle = .overFullScreen
+        self.present(mainTabBarController, animated: true, completion: nil)
+    }
 }
