03cd59571e1b21b7d3c1beedd27905d21296ca4f
thommzy <tomiwaobeisun@yahoo.com>
2021-11-03 10:39:13 +0100

    added some strings to constant file

--- TimJeffPizza/TimJeffPizza/Constants.swift
+++ TimJeffPizza/TimJeffPizza/Constants.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum Constants {
+    static let baseURL = "https://gist.githubusercontent.com/eliseo-juan"
+    static let isMarried = "isMarried"
+    static let isLoggedIn = "isLoggedIn"
+    static let main = "Main"
+    static let mainTabBarController = "MainTabBarController"
+    static let image = "image"
+    static let married = "Married"
+    static let single = "Single"
+    static let summary = "Summary"
+    static let appContinue = "Continue"
+    static let appCancel = "Cancel"
+    static let continueMessage = "Are you sure you want to continue?"
+}
+
+enum ViewControllers {
+    static let onBoardingViewController = "OnBoardingViewController"
+    static let summaryViewController = "SummaryViewController"
+}
