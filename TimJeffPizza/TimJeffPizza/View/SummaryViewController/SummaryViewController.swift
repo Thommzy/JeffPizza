ac03a5063962d8063ab864657a48a08011923c1e
thommzy <tomiwaobeisun@yahoo.com>
2021-11-10 12:46:56 +0100

    summary Vc

--- TimJeffPizza/TimJeffPizza/View/SummaryViewController/SummaryViewController.swift
+++ TimJeffPizza/TimJeffPizza/View/SummaryViewController/SummaryViewController.swift
@@ -11,7 +11,6 @@ class SummaryViewController: UIViewController {
 
     @IBOutlet weak var totalaAmountLabel: UILabel!
     @IBOutlet weak var statusLabel: UILabel!
-    @IBOutlet weak var pizzaNameLabel: UILabel!
     @IBOutlet weak var continueBtn: UIButton!
     
     var totalAmount: String?
@@ -21,20 +20,19 @@ class SummaryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = Constants.summary
+        title = NSLocalizedString("summary", comment: "This is summary Text.")
         setupDatas()
         setupContinueBtn()
     }
     
     
     func setupDatas() {
-        pizzaNameLabel.text = pizzaName
-        let userType = UserDefaults.standard.bool(forKey: Constants.isMarried)
+        let userType = UserDefaults.standard.bool(forKey: NSLocalizedString("isMarried", comment: "isMarried"))
         switch userType {
         case true:
-            statusLabel.text = Constants.married
+            statusLabel.text = NSLocalizedString("married", comment: "married")
         case false:
-            statusLabel.text = Constants.single
+            statusLabel.text = NSLocalizedString("single", comment: "single")
         }
         totalaAmountLabel.text = totalPrice
     }
@@ -49,18 +47,20 @@ class SummaryViewController: UIViewController {
     }
     
     @IBAction func continueBtnAction(_ sender: UIButton) {
-        let alert = UIAlertController(title: Constants.appContinue, message: Constants.continueMessage, preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: Constants.appContinue, style: UIAlertAction.Style.destructive, handler: { [weak self] action in
+        let alert = UIAlertController(title: NSLocalizedString("appContinue", comment: "appContinue"), message: NSLocalizedString("continueMessage", comment: "appContinue"), preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("appContinue", comment: "appContinue"), style: UIAlertAction.Style.destructive, handler: { [weak self] action in
             self?.setupMoveToMainScreen()
         }))
-        alert.addAction(UIAlertAction(title: Constants.appCancel, style: UIAlertAction.Style.cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: NSLocalizedString("appCancel", comment: "appCancel"), style: UIAlertAction.Style.cancel, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
     
     func setupMoveToMainScreen() {
-        let storyboard = UIStoryboard(name: Constants.main, bundle: nil)
-        let mainTabBarController = storyboard.instantiateViewController(identifier: Constants.mainTabBarController)
+        let storyboard = UIStoryboard(name: NSLocalizedString("main", comment: "main"), bundle: nil)
+        let mainTabBarController = storyboard.instantiateViewController(identifier: NSLocalizedString("mainTabBarController", comment: "mainTabBarController"))
         mainTabBarController.modalPresentationStyle = .overFullScreen
+        let data = CoreDataManager.shared.fetchData()
+        _ = data.map{CoreDataManager.shared.deleteData($0)}
         self.present(mainTabBarController, animated: true, completion: nil)
     }
 }
